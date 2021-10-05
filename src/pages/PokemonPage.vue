841c74e95d2bde92cf7a260dab502aa3b70e4fe6
Miriam Valera <miriamvdaw@gmail.com>
2021-10-05 17:10:44 +0200

    select pokemon random

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,11 +1,15 @@
 <template>
-    <h1>¿ Quién es este pokemon? </h1>
-    <!-- TODO: img -->
-    <PokemonPicture :pokemonId="26" :showPokemon="true" />
+    <h1 v-if="!pokemon">Espere por favor...</h1>
 
-    <!-- TODO: opciones -->
-    <PokemonOptions :pokemons="pokemonArr"/>
+    <div v-else>
+        <h1>¿ Quién es este pokemon? </h1>
 
+        <!-- TODO: img -->
+        <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
+
+        <!-- TODO: opciones -->
+        <PokemonOptions :pokemons="pokemonArr"/>
+    </div>
 </template>
 
 <script>
@@ -19,12 +23,17 @@ export default {
     components: { PokemonPicture, PokemonOptions },
     data() {
         return {
-            pokemonArr: []
+            pokemonArr: [],
+            pokemon: null,
+            showPokemon: false
         }
     },
     methods: {
         async mixPokemonArray() {
             this.pokemonArr = await getPokemonOptions()
+
+            const rndInt = Math.floor( Math.random() * 4 )
+            this.pokemon = this.pokemonArr[ rndInt ]
         }
     },
     mounted() {
