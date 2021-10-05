e2a450489efe0137f64a9114db118d6a257d830d
Miriam Valera <miriamvdaw@gmail.com>
2021-10-05 11:49:42 +0200

    first commit

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="pokemon-container">
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+         class="hidden-pokemon"
+         alt="pokemon">
+
+    <!-- <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+         class="fade-in"
+         alt="pokemon"> -->
+</div>
+    
+    
+</template>
+
+<script>
+export default {
+
+    computed: {
+        imgSrc() {
+            return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg`
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+.pokemon-container {
+    height: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+img {
+    height: 200px;
+    position: absolute;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
+
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>¿ Quién es este pokemon? </h1>
+    <!-- TODO: img -->
+    <PokemonPicture  :pokemonId="4"/>
+
+    <!-- TODO: opciones -->
+    <PokemonOptions />
+
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture'
+import PokemonOptions from '@/components/PokemonOptions'
+
+export default {
+    components: { PokemonPicture, PokemonOptions }
+}
+</script>
+
