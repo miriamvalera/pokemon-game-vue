e5372986404fed83d55eee7dfbbd500052398ed0
Miriam Valera <miriamvdaw@gmail.com>
2021-10-07 14:51:39 +0200

    add all unit-tests

---
+++ tests/unit/components/PokemonOptions.spec.js
@@ -0,0 +1,52 @@
+import { shallowMount } from '@vue/test-utils'
+import PokemonOptions from '@/components/PokemonOptions'
+
+import { pokemons } from '../mocks/pokemons.mock'
+
+describe('PokemonOptions Components', () => {
+
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = shallowMount( PokemonOptions, {
+            props: {
+                pokemons: pokemons
+            }
+        })
+    })
+    test('debe de hacer match con el snapshot', () => {
+
+        console.log(wrapper.html())
+        
+        expect( wrapper.html() ).toMatchSnapshot()
+    })
+
+    test('debe de mostrar las 4 opciones correctamente', () => {
+        
+        const liTags = wrapper.findAll('li')
+        expect( liTags.length ).toBe(4)
+
+        expect( liTags[0].text() ).toBe('pikachu')
+        expect( liTags[1].text() ).toBe('mew')
+        expect( liTags[2].text() ).toBe('venusaur')
+        expect( liTags[3].text() ).toBe('charmander')
+
+    })
+
+    test('debe de emitir "selection" con su respectivos parámetros al hacer click', () => {
+
+        const [ li1, li2, li3, li4 ] = wrapper.findAll('li')
+
+        li1.trigger('click')
+        li2.trigger('click')
+        li3.trigger('click')
+        li4.trigger('click')
+        // console.log(wrapper.emitted('selection-pokemon'))
+        expect( wrapper.emitted('selection-pokemon').length ).toBe(4)
+        expect( wrapper.emitted('selection-pokemon')[0]).toEqual([5])
+        expect( wrapper.emitted('selection-pokemon')[1]).toEqual([10])
+        expect( wrapper.emitted('selection-pokemon')[2]).toEqual([15])
+        expect( wrapper.emitted('selection-pokemon')[3]).toEqual([20])
+
+    })
+})
---
+++ tests/unit/mocks/pokemons.mock.js
@@ -0,0 +1,6 @@
+export const pokemons = [
+    { name: 'pikachu', id: 5 },
+    { name: 'mew', id: 10 },
+    { name: 'venusaur', id: 15 },
+    { name: 'charmander', id: 20 }
+]
